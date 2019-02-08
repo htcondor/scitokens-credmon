b9e66798bf0235306c85a4637ef6b244c14f7d44
Jason Patton <jpatton@cs.wisc.edu>
2019-02-08 09:24:20 -0600

    Create README.md and update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup, find_packages
 
-setup(name='scitokens-credmon',
-      version = '0.1',
-      description = 'Scitokens credential monitor for use with HTCondor',
-      url = 'https://github.com/htcondor/scitokens-credmon',
-      author = 'Jason Patton',
-      author_email = 'jpatton@cs.wisc.edu',
-      license = 'MIT',
-      packages = find_packages(),
-      scripts = ['bin/condor_credmon'],
-      install_requires = [
-          'htcondor',
-          'requests_oauthlib==1.0.0',
-          'six',
-          'flask'
-          ],
-      include_package_data = True
+setup(
+    name='scitokens-credmon',
+    version = '0.1',
+    description = 'Scitokens credential monitor for use with HTCondor',
+    long_description = open('README.md').read(),
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/htcondor/scitokens-credmon',
+    author = 'Jason Patton',
+    author_email = 'jpatton@cs.wisc.edu',
+    license = 'MIT',
+    packages = find_packages(),
+    scripts = ['bin/condor_credmon'],
+    install_requires = [
+        'htcondor >= 8.8.0',
+        'requests_oauthlib==1.0.0',
+        'six',
+        'flask'
+        ],
+    include_package_data = True
     )
